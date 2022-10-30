e858d886d14efede3092a02ff23288f6772a3e4b
ChinaFLTV <236010069@qq.com>
2022-10-30 14:15:27 +0800

    2022.10.30  14:15-Day5

---
+++ Day5/src/main/java/conf/SpringConfig.java
@@ -0,0 +1,20 @@
+package conf;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author LiGuanda
+ * @date 2022/10/30 AM 9:58:50
+ * @description no description right now...
+ * @filename SpringConfig.java
+ * @since a
+ */
+
+@Configuration
+@ComponentScan(value = {"bean"})
+public class SpringConfig {
+
+
+
+}
---
+++ Day5/src/main/java/service/Day5Service.java
@@ -0,0 +1,30 @@
+package service;
+
+import conf.SpringConfig;
+import org.junit.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+/**
+*
+* @author LiGuanda
+* @date 2022/10/30 AM 10:14:07
+* @description no description right now...
+* @filename Day5Service.java
+*
+*/
+
+
+
+public class Day5Service {
+
+
+    @Test
+    public void test(){
+
+    AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SpringConfig.class);
+
+
+
+    }
+
+}
